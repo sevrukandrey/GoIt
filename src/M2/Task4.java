40f0bbac05838b7c6c61b659f7396239a759d3ac
asevruk <asevruk@playtika.com>
2016-08-30 12:02:57 +0300

    try5

--- src/M2/Task4.java
+++ src/M2/Task4.java
@@ -4,19 +4,21 @@ package M2;
  * Created by asevruk on 8/26/2016.
  */
 public class Task4 {
+
+    static int[] balances = {1200, 250, 2000, 500, 3200};
+    static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
     public static void main(String[] args) {
-        double balance = 100;
-        double withdraw = 10000;
+
+        double fund = 100;
         String ownerName = "Ann";
-        System.out.println(ownerName + " " + fundBalance(ownerName, 200));
+        System.out.println(ownerName + " " + fundBalance(ownerName, fund));
 
     }
+
+
     static double fundBalance(String ownerName, double fund) {
-        int[] balances = {1200, 250, 2000, 500, 3200};
-        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
         double newBalance = 0;
-
-
         for (int i = 0; i < ownerNames.length; i++) {
 
             if (ownerNames[i].equals(ownerName)) {
