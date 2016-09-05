143cc794b8363cdbb9891460eca6dfbfb6aaadd0
asevruk <asevruk@playtika.com>
2016-09-05 16:24:35 +0300

    m3

--- src/M3/Task3/Main.java
+++ src/M3/Task3/Main.java
@@ -1,6 +1,7 @@
 package M3.Task3;
 
 
+import java.util.Date;
 
 /**
  * Created by asevruk on 8/31/2016.
@@ -9,9 +10,17 @@ public class Main {
 
 
 
+    public static void main(String[] args) {
+        Date fd = new Date(20,9,16);
+        System.out.println(fd);
+    }
+
+
+/*
     Course qa = new Course(10,"asd","asd");
     Course aqa = new Course();
     Course java = new Course();
     Course php = new Course();
     Course cPlusPlus = new Course();
+    */
 }
