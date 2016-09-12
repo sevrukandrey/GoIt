987aaba4862302b8146e5e81dad262330085ce26
Andrey Sevruk <sevruk_a@ukr.net>
2016-09-12 09:35:55 +0300

    as

--- src/M4/Task6/Main.java
+++ src/M4/Task6/Main.java
@@ -5,16 +5,21 @@ import static M4.Task6.Currency.*;
 /**
  * Created by asevruk on 9/6/2016.
  */
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-        User u1= new User(1,"Andrey",0,1,"Playtika",1000,new USBank(1,"USA", EUR,10,1200,1,100000));
-        User u2= new User(2,"Petya",1000,2,"Ciklum",2000,new USBank(1,"Canada", USD,10,1200,1,100000));
+        User u1 = new User(1, "Andrey", 0, 1, "Playtika", 1000, new USBank(1, "USA", EUR, 10, 1200, 1, 100000));
+        System.out.println(u1);
+        User u2 = new User(2, "Petya", 1000, 2, "Ciklum", 2000, new USBank(2, "Canada", USD, 10, 1200, 1, 100000));
+        System.out.println(u2);
 
-        User u3= new User(3,"Katya",2000,3,"SoftServ",3000,new EUBank(1,"Ukaraine", EUR,10,1200,1,100000));
-        User u4= new User(4,"Ira",3000,4,"ABB",1000,new EUBank(1,"Poland", EUR,10,1200,1,100000));
+        User u3 = new User(3, "Katya", 2000, 3, "SoftServ", 3000, new EUBank(3, "Ukaraine", EUR, 10, 1200, 1, 100000));
+        System.out.println(u3);
+        User u4 = new User(4, "Ira", 3000, 4, "ABB", 1000, new EUBank(1, "Poland", EUR, 10, 1200, 1, 100000));
+        System.out.println(u4);
 
-        User u5= new User(5,"Oran",4000,5,"DataArt",1000,new ChinaBank(1,"Croatia", EUR,10,1200,1,100000));
-        User u6= new User(6,"John",5000,6,"Funtime",1000,new ChinaBank(1,"Belarus", USD,10,1200,1,100000));
+        User u5 = new User(5, "Oran", 4000, 5, "DataArt", 1000, new ChinaBank(4, "Croatia", EUR, 10, 1200, 1, 100000));
+        System.out.println(u5);
+        User u6 = new User(6, "John", 5000, 6, "Funtime", 1000, new ChinaBank(5, "Belarus", USD, 10, 1200, 1, 100000));
+        System.out.println(u6);
     }
 }
